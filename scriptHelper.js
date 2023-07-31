26d8a32d898c8d027bf1ffd0fa90426763e71176
Bekah Temple <bekahtemple@MacBooks-MBP.attlocal.net>
2023-07-31 18:52:14 -0500

    started the assignment

--- scriptHelper.js
+++ scriptHelper.js
@@ -2,42 +2,56 @@
 require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
-                <h2>Mission Destination</h2>
-                <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
-                    <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
-                </ol>
-                <img src="">
-   */
+    // Here is the HTML formatting for our mission target div.
+    /*
+                 <h2>Mission Destination</h2>
+                 <ol>
+                     <li>Name: </li>
+                     <li>Diameter: </li>
+                     <li>Star: ${star}</li>
+                     <li>Distance from Earth: </li>
+                     <li>Number of Moons: </li>
+                 </ol>
+                 <img src="">
+    */
 }
 
-function validateInput(testInput) {
-   
+function validateInput(testInput) { //feels not done
+    if (typeof testInput === 'string') {
+        return testInput;
+    }
+    if (typeof testInput === 'number') {
+        return "Is a Number";
+    }
+    if (testInput === '') {
+        return "Empty";
+    }
+    if (isNaN(testInput) === true) {
+        return "Not a Number";
+    }
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+    
 }
 
-async function myFetch() {
+async function myFetch() { //feels done
     let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json")
+        .then(function (response) {
+            return response.json();
         });
 
     return planetsReturned;
 }
 
-function pickPlanet(planets) {
+function pickPlanet(planets) { //done
+    return planets[Math.floor(Math.random() * planets.length)];
 }
 
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
 module.exports.formSubmission = formSubmission;
-module.exports.pickPlanet = pickPlanet; 
+module.exports.pickPlanet = pickPlanet;
 module.exports.myFetch = myFetch;
