7dd856ddec86750128c4fe1f843aaed4058ae7d2
Bekah Temple <bekahtemple@MacBooks-MBP.attlocal.net>
2023-08-02 21:22:36 -0500

    fixed typos for statuses

--- scriptHelper.js
+++ scriptHelper.js
@@ -31,38 +31,53 @@ function validateInput(testInput) {//feels not done
 
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-    let pilotName = document.getElementByID('pilotName');
-    let copilotName = document.getElementByID('copilotName');
-    let fuel = document.getElementById('fuelLevel');
-    let cargo = document.getElementByID('cargoLevel');
+    // variables
+    // let pilotName = document.getElementByID('pilotName');
+    // let copilotName = document.getElementByID('copilotName');
+    // let fuel = document.getElementById('fuelLevel');
+    // let cargo = document.getElementByID('cargoLevel');
+    let launchStatus = document.getElementById('launchStatus');
+    let pilotStatus = document.getElementById('pilotStatus');
+    let copilotStatus = document.getElementById('copilotStatus');
+    let cargoStatus = document.getElementById('cargoStatus');
+    let fuelStatus = document.getElementById('fuelStatus');
+    let faultyItems = document.getElementById('faultyItems');
+
     //validate pilot & copilot as string
     //validate fuel & cargo as number
+
     if (validateInput(pilot) === "Empty" || validateInput(copilot) === "Empty" || validateInput(fuelLevel) === "Empty" || validateInput(cargoLevel) === "Empty") {
         alert("All fields are required!");
+
     } else if (validateInput(pilot) === "Is a Number" || validateInput(copilot) === "Is a Number" || validateInput(fuelLevel) === "Not a Number" || validateInput(cargoLevel) === "Not a Number") {
         alert("Make sure to enter valid information for each field!");
-        //at this point all fields valid, then:
+
+        //at this point all fields valid input, then:
         //check fuel level
         //check cargo mass
 
     } else if (cargoLevel > 10000 || fuelLevel < 10000) {
-        list.style.vsibility = "visible";
-        document.getElementByID("launchStatus").innerHTML = "Shuttle not ready for launch"; //also change to red
-        document.getElementByID("pilotStatus").innerHTML = `Pilot ${pilot} is ready for launch`;
-        document.getElementByID("copilotStatus").innerHTML = `Pilot ${copilot} is ready for launch`;
-        if (fuelLevel < 10000) {
-            document.getElementByID("fuelStatus").innerHTML = `Fuel level too low for launch`
+        faultyItems.style.vsibility = "visible"; //makes list visible
+        launchStatus.innerHTML = "Shuttle Not Ready for Launch"; 
+        launchStatus.style.color = 'rgb(199, 37, 78)'; //turn red
+        pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
+        copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
+        if (fuelLevel < 10000) { //too little fuel
+            fuelStatus.innerHTML = `Fuel level too low for launch`
         } else {
-            document.getElementByID("fuelStatus").innerHTML = `Fuel level high enough for launch`
+            fuelStatus.innerHTML = `Fuel level high enough for launch`
         }
-        if (cargoLevel > 10000) {
-            document.getElementByID("cargoStatus").innerHTML = `Cargo mass too high for launch`
+        if (cargoLevel > 10000) { //too much cargo
+            cargoStatus.innerHTML = `Cargo mass too heavy for launch`
         } else {
-            document.getElementByID("cargoStatus").innerHTML = `Cargo mass low enough for launch`
+            cargoStatus.innerHTML = `Cargo mass low enough for launch`
         }
+
+        //successful launch
+
     } else {
-        //success
-        document.getElementByID("launchStatus").innerHTML = `Shuttle is ready for Launch`
+        launchStatus.innerHTML = `Shuttle is Ready for Launch`
+        launchStatus.style.color = 'rgb(65, 159, 106)';
     }
 
 
